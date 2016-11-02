9a4b7f930ccfc2f6ae4834204bd8177ac79623a0
anhhtbk <anhhtbk@gmail.com>
2016-11-02 17:20:27 +0700

    add source

---
+++ ThueXeToanCau/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThueXeToanCau
+//
+//  Created by VMio69 on 02/11/2016.
+//  Copyright © 2016 AnhHT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThueXeToanCau/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ThueXeToanCau
+//
+//  Created by VMio69 on 02/11/2016.
+//  Copyright © 2016 AnhHT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
